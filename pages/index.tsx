9165b61f51d0b7d338f775ec5b2c34da9d093987
mhmdho <hadiomidm@gmail.com>
2022-03-30 16:11:53 +0430

    Add user and transaction schema

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 const Home: NextPage = () => {
-  return (
+  return (0
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
         <title>Create Next App</title>
