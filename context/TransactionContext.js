6c5c314225788a6bc0b727d2e050100c3a9b1aba
mhmdho <hadiomidm@gmail.com>
2022-04-02 11:32:14 +0430

    Edit TransactionContext.js

--- context/TransactionContext.js
+++ context/TransactionContext.js
@@ -11,11 +11,31 @@ if (typeof window !== 'undefined') {
 export const TransactionProvider = ({ children }) => {
   const [currentAccount, setCurrentAccount] = useState()
 
+  useEffect(() => {
+    checkIfWalletIsConnected()
+  }, [])
+
   const connectWallet = async (metamask = eth) => {
     try {
       if (!metamask) return alert('Please install metamask ')
       const accounts = await metamask.request({ method: 'eth_requestAccounts' })
       setCurrentAccount(accounts[0])
+    } catch (error) {
+      console.error(error)
+      throw new Error('No ethereum object.')
+    }
+  }
+
+  const checkIfWalletIsConnected = async (metamask = eth) => {
+    try {
+      if (!metamask) return alert('Please install metamask ')
+
+      const accounts = await metamask.request({ method: 'eth_accounts' })
+
+      if (accounts.length) {
+        setCurrentAccount(accounts[0])
+        console.log('Wallet is already connected!')
+      }
     } catch (error) {
       console.error(error)
       throw new Error('No ethereum object.')
