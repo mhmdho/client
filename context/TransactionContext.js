547abd2ea3da751033c672495c9c336f688f5597
mhmdho <hadiomidm@gmail.com>
2022-04-06 17:47:59 +0430

    TransactionContext.js: Add sendTransaction & handleChange, Edit TransactionProvider

--- context/TransactionContext.js
+++ context/TransactionContext.js
@@ -10,6 +10,12 @@ if (typeof window !== 'undefined') {
 
 export const TransactionProvider = ({ children }) => {
   const [currentAccount, setCurrentAccount] = useState()
+  const [isLoading, setIsLoading] = useState(false)
+  const router = useRouter()
+  const [formData, setFormData] = useState({
+    addressTo: '',
+    amount: '',
+  })
 
   useEffect(() => {
     checkIfWalletIsConnected()
@@ -42,11 +48,65 @@ export const TransactionProvider = ({ children }) => {
     }
   }
 
+  const sendTransaction = async (
+    metamask = eth,
+    connectedAccount = currentAccount,
+  ) => {
+    try {
+      if (!metamask) return alert('Please install metamask ')
+      const { addressTo, amount } = formData
+      const transactionContract = getEthereumContract()
+
+      const parsedAmount = ethers.utils.parseEther(amount)
+
+      await metamask.request({
+        method: 'eth_sendTransaction',
+        params: [
+          {
+            from: connectedAccount,
+            to: addressTo,
+            gas: '0x7EF40', // 520000 Gwei
+            value: parsedAmount._hex,
+          },
+        ],
+      })
+  
+      const transactionHash = await transactionContract.publishTransaction(
+        addressTo,
+        parsedAmount,
+        `Transferring ETH ${parsedAmount} to ${addressTo}`,
+        'TRANSFER',
+      )
+
+      setIsLoading(true)
+
+      await transactionHash.wait()
+
+       // DB
+      // await saveTransaction(
+      //   transactionHash.hash,
+      //   amount,
+      //   connectedAccount,
+      //   addressTo,
+      // )
+
+      setIsLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleChange = (e, name) => {
+    setFormData(prevState => ({ ...prevState, [name]: e.target.value }))
+  }
+
   return (
     <TransactionContext.Provider
       value={{
         currentAccount,
         connectWallet,
+        sendTransaction,
+        handleChange,
       }}
     >
       {children}
